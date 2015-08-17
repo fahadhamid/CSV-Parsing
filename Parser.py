a2465980b1c61513d6c102e40217d6abd1f5b54b
irtzaqc <irtza.qc@gmail.com>
2015-08-17 10:11:33 +0500

    Updated code to Pylint standards Refactored into proper OOP classes

--- Parser.py
+++ Parser.py
@@ -1,70 +1,72 @@
+"""parses the file and displayes each individual record"""
 __author__ = 'root'
 import csv
 
-# input_file = csv.DictReader(open())
-# x = 0;
-# for row in input_file:
-#     print row
-#     x+=1
-#     if(x==6):
-#         break
 with open('bone density example.csv', 'rb') as csvfile:
-    dialect = csv.Sniffer().sniff(csvfile.read(1024))
+    DIALECT = csv.Sniffer().sniff(csvfile.read(1024))
     csvfile.seek(0)
-    reader = csv.reader(csvfile, dialect)
-    l_reader = list(reader)
-    new_list = list()
-    for row in l_reader:
+    READER = csv.reader(csvfile, DIALECT)
+    LIST_READER = list(READER)
+    NEW_LIST = list()
+    for row in LIST_READER:
         if not (row[0] == 'Patient Name' or str(row).__contains__('Page')):
-            new_list.append(row)
+            NEW_LIST.append(row)
 
-for row in new_list:
-    if row[0] == '' and new_list[new_list.index(row) - 1][0] == '' and new_list[new_list.index(row) - 2][0] != '':
-        
-        print "/*****************USER*****************/ \n"
-        print "Name: " + str(new_list[new_list.index(row) - 2][0]).upper()
-        print "Gender: " + str(new_list[new_list.index(row) - 2][2]).upper()
-        print "DOB: " + str(new_list[new_list.index(row) - 2][3]).upper()
-        print "Language: " + str(new_list[new_list.index(row) - 2][4]).upper()
-        print "Account Number: " + str(new_list[new_list.index(row) - 2][5]).upper()
-        print "Race: " + str(new_list[new_list.index(row) - 2][6]).upper()
-        print "Ethnicity: " + str(new_list[new_list.index(row) - 2][7]).upper()
-        print "Phone Number: " + str(new_list[new_list.index(row) - 2][8]).upper()
-        print "Reminder Method: " + str(new_list[new_list.index(row) - 2][14])
-        if str(new_list[new_list.index(row) - 2][10]) == '':
-            print "Email: N/A"
+
+def display_details(current_row, new_list):
+    """displays the details except for address"""
+    print "/*****************USER*****************/ \n"
+    print "Name: " + \
+          str(new_list[new_list.index(current_row) - 2][0]).upper()
+    print "Gender: " \
+          + str(new_list[new_list.index(current_row) - 2][2]).upper()
+    print "DOB: " + \
+          str(new_list[new_list.index(current_row) - 2][3]).upper()
+    print "Language: " + \
+          str(new_list[new_list.index(current_row) - 2][4]).upper()
+    print "Account Number: " + \
+          str(new_list[new_list.index(current_row) - 2][5]).upper()
+    print "Race: " + \
+          str(new_list[new_list.index(current_row) - 2][6]).upper()
+    print "Ethnicity: " + \
+          str(new_list[new_list.index(current_row) - 2][7]).upper()
+    print "Phone Number: " + \
+          str(new_list[new_list.index(current_row) - 2][8]).upper()
+    print "Reminder Method: " + \
+          str(new_list[new_list.index(current_row) - 2][14])
+
+    if str(NEW_LIST[NEW_LIST.index(current_row) - 2][10]) == '':
+        print "Email: N/A"
+    else:
+        print "Email: " \
+              "" + str(NEW_LIST[NEW_LIST.index(current_row) - 2][10]).lower()
+
+
+for row in NEW_LIST:
+    if row[0] == '' and NEW_LIST[NEW_LIST.index(row) - 1][0] == '' \
+            and NEW_LIST[NEW_LIST.index(row) - 2][0] != '':
+
+        display_details(row, NEW_LIST)
+
+        if str(NEW_LIST[NEW_LIST.index(row) - 2][12]).__contains__("returned") \
+                or str(NEW_LIST[NEW_LIST.index(row) - 2][12]).__contains__("RTN"):
+            print "Adress: " + str(NEW_LIST[NEW_LIST.index(row) - 1][12]).upper() + " " + str(
+                row[12]).upper() + " " + str(NEW_LIST[NEW_LIST.index(row) - 2][13]).upper() + " " + str(
+                NEW_LIST[NEW_LIST.index(row) - 1][13]).upper() + " " + str(row[13]).upper()
         else:
-            print "Email: " + str(new_list[new_list.index(row) - 2][10]).lower()
-        if str(new_list[new_list.index(row) - 2][12]).__contains__("returned") or str(
-                new_list[new_list.index(row) - 2][12]).__contains__("RTN"):
-            print "Adress: " + str(new_list[new_list.index(row) - 1][12]).upper() + " " + str(
-                row[12]).upper() + " " + str(new_list[new_list.index(row) - 2][13]).upper() + " " + str(
-                new_list[new_list.index(row) - 1][13]).upper() + " " + str(row[13]).upper()
-        else:
-            print "Adress: " + str(new_list[new_list.index(row) - 2][12]).upper() + " " + str(
-                new_list[new_list.index(row) - 1][12]).upper() + " " + str(row[12]).upper() + " " + str(
-                new_list[new_list.index(row) - 2][13]).upper() + " " + str(
-                new_list[new_list.index(row) - 1][13]).upper() + " " + str(row[13]).upper()
+            print "Adress: " + str(NEW_LIST[NEW_LIST.index(row) - 2][12]).upper() + " " + str(
+                NEW_LIST[NEW_LIST.index(row) - 1][12]).upper() + " " + str(row[12]).upper() + " " + str(
+                NEW_LIST[NEW_LIST.index(row) - 2][13]).upper() + " " + str(
+                NEW_LIST[NEW_LIST.index(row) - 1][13]).upper() + " " + str(row[13]).upper()
         print "/*****************END USER*****************/ \n"
 
-    elif row[0] != '' and new_list[new_list.index(row) - 1][0] == '' and new_list[new_list.index(row) - 2][0] != '':
+    elif row[0] != '' and NEW_LIST[NEW_LIST.index(row) - 1][0] == '' \
+            and NEW_LIST[NEW_LIST.index(row) - 2][0] != '':
 
-        print "/*****************USER*****************/ \n"
-        print "Name: " + str(new_list[new_list.index(row) - 2][0]).upper()
-        print "Gender: " + str(new_list[new_list.index(row) - 2][2]).upper()
-        print "DOB: " + str(new_list[new_list.index(row) - 2][3]).upper()
-        print "Language: " + str(new_list[new_list.index(row) - 2][4]).upper()
-        print "Account Number: " + str(new_list[new_list.index(row) - 2][5]).upper()
-        print "Race: " + str(new_list[new_list.index(row) - 2][6]).upper()
-        print "Ethnicity: " + str(new_list[new_list.index(row) - 2][7]).upper()
-        print "Phone Number: " + str(new_list[new_list.index(row) - 2][8]).upper()
-        print "Reminder Method: " + str(new_list[new_list.index(row) - 2][14])
-        if str(new_list[new_list.index(row) - 2][10]) == '':
-            print "Email: N/A"
-        else:
-            print "Email: " + str(new_list[new_list.index(row) - 2][10]).lower()
+        display_details(row, NEW_LIST)
 
-        print "Adress: " + str(new_list[new_list.index(row) - 2][12]).upper() + " " + str(
-            new_list[new_list.index(row) - 1][12]).upper() + " " + str(
-            new_list[new_list.index(row) - 2][13]).upper() + " " + str(new_list[new_list.index(row) - 1][13]).upper()
+        print "Adress: " + str(NEW_LIST[NEW_LIST.index(row) - 2][12]).upper() + " " + str(
+            NEW_LIST[NEW_LIST.index(row) - 1][12]).upper() + " " + str(
+            NEW_LIST[NEW_LIST.index(row) - 2][13]).upper() + " " + \
+              str(NEW_LIST[NEW_LIST.index(row) - 1][13]).upper()
         print "/*****************END USER*****************/ \n"
